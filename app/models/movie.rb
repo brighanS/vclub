bb2c8813d63d61a939024d34b201fbff7f6949cc
Brighan <brighan@homie.mx>
2018-06-25 17:00:44 -0500

    Add valdiations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+
+  validates :name, :genre, :image_url, :price, :duration, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :name, uniqueness: true
+  validates :name, length: {maximum: 80, message: 'cannot exceed 80 chars'}
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|png|jpg)\Z}i,
+    message: 'must be a URL for .gif, .png or .jpg image'}
+
 end
