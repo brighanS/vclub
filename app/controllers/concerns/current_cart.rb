2973cbde45590cb4dc4f222d9f86c5821a496cdb
Brighan <brighan@homie.mx>
2018-06-26 17:32:46 -0500

    Fix issue when creating the first cart for user

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -7,6 +7,6 @@ module CurrentCart
     #@cart = Cart.find_by cart_id: current_user
   rescue ActiveRecord::RecordNotFound
     @cart = Cart.create
-    @cart.id = current_user.id
+   current_user.id = @cart.id
   end
 end
