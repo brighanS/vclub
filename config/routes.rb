5a9d29d57d5b637b2f9935e908c2fc5811e7cb66
Brighan <brighan@homie.mx>
2018-06-26 09:45:38 -0500

    Setup devise manually

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'landing/selector'
   resources :movies
 
-  root 'movies#index', as: 'landing_index'
+  root to: 'movies#index'
 
   get 'rent_a_movie', to: 'movies#make_a_rent'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
