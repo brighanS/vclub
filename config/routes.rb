58bfd4916984d006658781b00700eebd129d19f0
Brighan <brighan@homie.mx>
2018-06-26 11:36:34 -0500

    Add authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
   get 'landing/selector'
   resources :movies
-
   root to: 'movies#index'
-
   get 'rent_a_movie', to: 'movies#make_a_rent'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
