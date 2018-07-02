e0ac59e34d7e109528a7f5bd1cba7a512e7b2d63
Brighan <brighan@homie.mx>
2018-07-02 16:46:36 -0500

    fix user sessions and carts

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,9 +3,9 @@ module CurrentCart
   private
 
   def set_cart
-    @cart = Cart.find(current_user.id)
+    @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
     @cart = Cart.create
-   # @cart.id = current_user.id 
+  session[:cart_id] = @cart.id
   end
 end
