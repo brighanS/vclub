09f76a83180b3bac3ee405230f897ef5e6695981
Brighan <brighan@homie.mx>
2018-07-03 12:23:09 -0500

    Fix form styles for users: login, signup and forgotPassword. Movies

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   root 'movies#index'
   get 'rent_a_movie', to: 'movies#make_a_rent'
   get 'index', to: 'movies#index'
+  get 'users', to: 'movies#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
